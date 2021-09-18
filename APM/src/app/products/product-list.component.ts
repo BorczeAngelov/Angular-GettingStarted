54574a4e6b16917ad3f16b70abc0ec3fb8044adb
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-09-18 19:48:18 +0200

    add event binding and message between components

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -38,4 +38,8 @@ export class ProductListComponent{
       toggleImage(): void {
         this.showImage = !this.showImage;
       }
+
+      onRatingClicked(message: string) : void{
+        this.pageTitle = 'Product List ' + message;
+      }
 }
