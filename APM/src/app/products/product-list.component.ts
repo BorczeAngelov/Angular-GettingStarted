d00eed8faaabe326b1f1e1876f02c4f6aa8f1292
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-09-19 14:26:44 +0200

    build service and use dependency injection

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,45 +1,49 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html'
 })
 
-export class ProductListComponent{
-    pageTitle: string = 'Product List'    
-    imageWidth: number = 50;
-    imageMargin: number = 2;
-    showImage: boolean = false;
-    listFilter: string ='cart';
+export class ProductListComponent implements OnInit {
+  pageTitle: string = 'Product List'
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  
+  private _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] = [];
 
 
-    products: any[] = [  
-        {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2021",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "assets/images/leaf_rake.png"
-      },
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2021",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png"
-      }];
+  constructor(private productService: ProductService) { }
 
-      toggleImage(): void {
-        this.showImage = !this.showImage;
-      }
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
+  }
 
-      onRatingClicked(message: string) : void{
-        this.pageTitle = 'Product List ' + message;
-      }
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy));
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List ' + message;
+  }
 }
