91e05d958ba2ebcd9edecd55bf593080b02c2559
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-09-22 22:51:38 +0200

    extend product module

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StartComponent } from '../shared/star.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductListComponent } from './product-list.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent,
+    StartComponent,
+    ProductDetailComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      {
+        path: 'products/:id',
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent
+      },
+    ])
   ]
 })
 export class ProductModule { }
