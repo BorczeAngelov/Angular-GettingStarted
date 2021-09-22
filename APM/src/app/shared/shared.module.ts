2d6a9874f359c8181802fb54b9d9b346ab7da044
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-09-22 22:57:05 +0200

    create shared.module and modify product.module

---
+++ APM/src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StartComponent } from './star.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    StartComponent,],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    FormsModule,
+    StartComponent,
+    CommonModule,
+  ]
+})
+export class SharedModule { }
